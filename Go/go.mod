bab60124c5b552f05673d501426232b689fad844
HackTheOxidation <tomas.hagenau@protonmail.ch>
2024-08-09 22:16:34 +0200

    Solve the go challenge

--- Go/go.mod
+++ Go/go.mod
@@ -2,4 +2,4 @@ module parrot
 
 go 1.12
 
-require github.com/stretchr/testify v1.6.1
+require github.com/stretchr/testify v1.9.0
--- Go/parrot.go
+++ Go/parrot.go
@@ -1,7 +1,7 @@
 package parrot
 
 import (
-	"errors"
+  "fmt"
 	"math"
 )
 
@@ -19,63 +19,78 @@ type Parrot interface {
 	Cry() (string, error)
 }
 
-type mixedParrot struct {
-	_type            parrotType
+type parrotTraits struct {
 	numberOfCoconuts int
 	voltage          float64
 	nailed           bool
 }
 
-func CreateParrot(t parrotType, numberOfCoconuts int, voltage float64, nailed bool) Parrot {
-	return mixedParrot{t, numberOfCoconuts, voltage, nailed}
-}
-
-func (parrot mixedParrot) Speed() (float64, error) {
-	switch parrot._type {
-	case TypeEuropean:
-		return parrot.baseSpeed(), nil
-
-	case TypeAfrican:
-		return math.Max(0, parrot.baseSpeed()-parrot.loadFactor()*float64(parrot.numberOfCoconuts)), nil
-
-	case TypeNorwegianBlue:
-		if parrot.nailed {
-			return 0, nil
-		}
-		return parrot.computeBaseSpeedForVoltage(parrot.voltage), nil
-
-	default:
-		return 0, errors.New("should be unreachable")
-	}
-}
-
-func (parrot mixedParrot) computeBaseSpeedForVoltage(voltage float64) float64 {
-	return math.Min(24.0, voltage*parrot.baseSpeed())
-}
-
-func (parrot mixedParrot) loadFactor() float64 {
+func (parrot parrotTraits) loadFactor() float64 {
 	return 9.0
 }
 
-func (parrot mixedParrot) baseSpeed() float64 {
+func (parrot parrotTraits) baseSpeed() float64 {
 	return 12.0
 }
 
-func (parrot mixedParrot) Cry() (string, error) {
-	switch parrot._type {
-	case TypeEuropean:
-		return "Sqoork!", nil
+func (parrot parrotTraits) computeBaseSpeedForVoltage(voltage float64) float64 {
+	return math.Min(24.0, voltage * parrot.baseSpeed())
+}
 
-	case TypeAfrican:
-		return "Sqaark!", nil
+type EuropeanParrot struct {
+  parrotTraits
+}
 
-	case TypeNorwegianBlue:
-		if parrot.voltage > 0 {
-			return "Bzzzzzz", nil
-		}
-		return "...", nil
+func (parrot EuropeanParrot) Speed() (float64, error) {
+	return parrot.baseSpeed(), nil
+}
 
-	default:
-		return "", errors.New("should be unreachable")
-	}
+func (parrot EuropeanParrot) Cry() (string, error) {
+	return "Sqoork!", nil
+}
+
+type AfricanParrot struct {
+  parrotTraits
+}
+
+func (parrot AfricanParrot) Speed() (float64, error) {
+	return math.Max(0, parrot.baseSpeed() - parrot.loadFactor() * float64(parrot.numberOfCoconuts)), nil
+}
+
+func (parrot AfricanParrot) Cry() (string, error) {
+	return "Sqaark!", nil
+}
+
+type NorwegianBlueParrot struct {
+  parrotTraits
+}
+
+func (parrot NorwegianBlueParrot) Speed() (float64, error) {
+	if parrot.nailed {
+		return 0, nil
+	} else {
+		return parrot.computeBaseSpeedForVoltage(parrot.voltage), nil
+  }
+}
+
+func (parrot NorwegianBlueParrot) Cry() (string, error) {
+	if parrot.voltage > 0 {
+		return "Bzzzzzz", nil
+	} else {
+	  return "...", nil
+  }
+}
+
+func CreateParrot(t parrotType, numberOfCoconuts int, voltage float64, nailed bool) (Parrot, error) {
+  traits := parrotTraits{numberOfCoconuts, voltage, nailed}
+  switch t {
+  case TypeEuropean:
+    return EuropeanParrot{traits}, nil
+  case TypeAfrican:
+    return AfricanParrot{traits}, nil
+  case TypeNorwegianBlue:
+    return NorwegianBlueParrot{traits}, nil
+  default:
+    return nil, fmt.Errorf("Unknown parrot type passed")
+  }
 }
